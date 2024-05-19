0facbfcf49d5263e81723441968ba6dccd8b2ad1
Nikita Glazov <niglazov@gmail.com>
2024-05-19 23:57:44 +0300

    add lab

--- sim/tests/common/riscv_macros.h
+++ sim/tests/common/riscv_macros.h
@@ -101,7 +101,10 @@
 
 #define INTERRUPT_HANDLER j other_exception /* No interrupts should occur */
 
-#define RVTEST_CODE_BEGIN                                               \
+#define RVTEST_CODE_BEGIN               \
+        .org 0x2000,0;
+        MSG_TRAP:                                                       \
+        .string "envcall"; 
         .section .text.init;                                            \
         .org 0xC0, 0x00;                                                \
         .balign  64;                                                    \
@@ -116,6 +119,16 @@ trap_vector:                                                            \
         beq a4, a5, _report;                                            \
         li a5, CAUSE_MACHINE_ECALL;                                     \
         beq a4, a5, _report;                                            \
+        /* init for loop, 0xf0000000 address for print */ \
+        lui a6, 0xf0000; \
+        la a2, MSG_TRAP; \
+        next_iter: \
+        lb a3, 0(a2); \
+        beq a3, x0, break_from_loop; \
+        sw a3, 0(a6); /* write to a6 char for print */ \
+        addi a2, a2, 1; \
+        jal x0,next_iter; \
+        break_from_loop: \
         /* if an mtvec_handler is defined, jump to it */                \
         la a4, mtvec_handler;                                           \
         beqz a4, 1f;                                                    \
